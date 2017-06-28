093d2df557f44cbd24548c4e0a9819d623e198f3
leeyunfan <leeyunfans@163.com>
2017-06-29 01:38:56 +0800

    修改

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/VojtechVitek/go-trello"
 	"github.com/faaaar/R/util"
 	"log"
+	"time"
 )
 
 func main() {
@@ -22,28 +23,61 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	userid := user.Id
 
 	boards, err := user.Boards()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	if len(boards) > 0 {
-		board := boards[0]
-		log.Printf("* %v (%v)\n", board.Name, board.ShortUrl)
+	startDay := time.Now()
 
-		lists, err := board.Lists()
+	for startDay.Weekday() != time.Monday {
+		startDay = startDay.AddDate(0, 0, -1)
+	}
+
+	startStr := startDay.Format("2006-01-02")
+	startF, _ := time.Parse("2006-01-02", startStr)
+	startTs := startF.Unix()
+
+	log.Println(startTs)
+	for _, board := range boards {
+		boardName := board.Name
+		log.Printf("* %v (%v)\n", boardName, board.ShortUrl)
+
+		cards, err := board.Cards()
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		for _, list := range lists {
-			log.Println("   - ", list.Name)
+		for _, card := range cards {
+			isOwnCard := false
+			memberIdList := card.IdMembers
+			for _, id := range memberIdList {
+				if id == userid {
+					isOwnCard = true
+					break
+				}
+			}
 
-			cards, _ := list.Cards()
-			for _, card := range cards {
-				log.Println("      + ", card.Name)
+			if isOwnCard {
+				if card.Due != "" {
+					t, err := time.Parse(time.RFC3339, card.Due)
+					if err != nil {
+						log.Fatalln(err)
+					}
+
+					log.Println(t.Weekday(), time.Now().Unix())
+				}
 			}
 		}
+		// for _, list := range lists {
+		//   log.Println("   - ", list.Name)
+		//
+		//   cards, _ := list.Cards()
+		//   for _, card := range cards {
+		//     log.Println("      + ", card.Name)
+		//   }
+		// }
 	}
 }
